16b86c14911908e2387e1e4643727bc75b83918a
gowthamDepaul <55822808+gowthamDepaul@users.noreply.github.com>
2020-10-26 00:41:22 -0500

    front end add code for handling alert and authguard
    alert and auth.guard and tsconfig adds

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,16 +3,39 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  public loginInvalid: boolean;
+  private formSubmitAttempt: boolean;
+  private returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router  
+  ) { }
 
   ngOnInit(): void {
   }
-
+  
+  async onSubmit() {
+    this.loginInvalid = false;
+    this.formSubmitAttempt = false;
+    if (this.form.valid) {
+      try {
+        const username = this.form.get('username').value;
+        const password = this.form.get('password').value;
+      } catch (err) {
+        this.loginInvalid = true;
+      }
+    } else {
+      this.formSubmitAttempt = true;
+    }
+  }
 }
