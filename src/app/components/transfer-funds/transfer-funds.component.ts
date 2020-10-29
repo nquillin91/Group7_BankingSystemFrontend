ed2839d799e760bee4aefde6cdd1357c14b0a195
ashaik16 <ashaik16@depaul.edu>
2020-10-29 13:35:19 -0500

    transfer-funds component added

---
+++ src/app/components/transfer-funds/transfer-funds.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { FormGroup, FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-transfer-funds',
+  templateUrl: './transfer-funds.component.html',
+  styleUrls: ['./transfer-funds.component.css']
+})
+export class TransferFundsComponent implements OnInit {
+
+  AccountTypes: any = ['Savings', 'Checking'];
+
+  // constructor(public dialog: MatDialog) {}
+
+  // openDialog() {
+  //   const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log(`Dialog result: ${result}`);
+  //   });
+  // }
+  transferFundsForms = new FormGroup({
+
+    senderName : new FormControl(),
+    accountType : new FormControl(),
+    senderAccountNumber : new FormControl(),
+    accountBalance : new FormControl(),
+    receiverName : new FormControl(),
+    receiverAccountNumber : new FormControl(),
+    transferAmount :  new FormControl(),
+    date : new FormControl(),
+    remarks : new FormControl(),
+   });
+
+  ngOnInit(): void {
+  }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    const formValue = this.transferFundsForms.value;
+  }
+}
+
+// @Component({
+//   selector: 'dialog-content-example-dialog',
+//   templateUrl: 'dialog-content-example-dialog.html',
+// })
+// export class DialogContentExampleDialog {}
