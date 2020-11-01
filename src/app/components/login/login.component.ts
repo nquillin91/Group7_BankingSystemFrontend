ddb464dfea0231af23debe3aed8088ebb0558995
gowthamDepaul <55822808+gowthamDepaul@users.noreply.github.com>
2020-11-01 14:02:23 -0600

    pull info
    pull info

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -35,7 +35,7 @@ export class LoginComponent implements OnInit {
         this.router.navigateByUrl('/home');
       },
       error => {
-        //this.internalServerError = true;
+        alert("Username password combination does not exsist")
       }
     );
   }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
 import { AuthenticationService } from '../../_services/authentication.service';
+import { UserProfile } from '../../models/userprofile';
+import { UserService } from '../../_services/user.service';
+import { Router } from '@angular/router';
+import { observable } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -13,27 +17,66 @@ export class RegisterComponent implements OnInit {
   myForm: FormGroup;
   error = false;
   errorMessage = '';
+  userProfile: UserProfile;
+    birthDate: FormControl;
+    existingPassword: string;
+    newPassword: string;
 
   Roles: any = ['Admin', 'Banker', 'Employee'];
 
-  constructor(private fb: FormBuilder, public authenticationService: AuthenticationService) { }
+  constructor(private fb: FormBuilder, public authenticationService: AuthenticationService, private userService: UserService, private router: Router) {
+    this.userProfile = new UserProfile();
+    this.birthDate = new FormControl(new Date(this.userProfile.birthDate));
+}
+
+  // ngOnInit() {
+  //   this.myForm = this.fb.group({
+  //     email: ['', Validators.compose([
+  //     Validators.required,
+  //     this.isEmail
+  //     ])],
+  //     password: ['', Validators.required],
+  //     confirmPassword: ['', Validators.compose([
+  //     Validators.required,
+  //     this.isEqualPassword.bind(this)
+  //     ])],
+  //     });
+  // }
 
   ngOnInit() {
-    this.myForm = this.fb.group({
-      email: ['', Validators.compose([
-      Validators.required,
-      this.isEmail
-      ])],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.compose([
-      Validators.required,
-      this.isEqualPassword.bind(this)
-      ])],
-      });
-  }
 
-  SignUp(){
-    alert("user info stored in firebase")
+    var jsonval={
+      "username" : "testuser",
+      "password" : "testPass",
+      "firstName" : "Nicholas",
+      "middleName" : "Ryan",
+      "lastName" : "Quillin",
+        "birthDate" : "02/05/1995",
+        "providedIncome" : 78000,
+      "phoneNumber" : "708-525-1444",
+      "emailAddress" : "nquillin91@gmail.com",
+      "ssn" : "123-12-1234",
+      "addressLine1" : "123 Test Avenue",
+      "addressLine2" : "1",
+      "city" : "Chicago",
+      "state" : "IL",
+      "zipcode" : "60602"
+    };
+
+    this.SignUp(jsonval);
+}
+
+  SignUp(val){
+    console.log("Test")
+    alert("user info stored in firebase");
+    this.authenticationService.signup(val).subscribe(
+      res => {
+        this.router.navigateByUrl('/login');
+      },
+      error => {
+        alert("Error on creation on user")
+      }
+    );
     //this.authenticationService.signup(RegistrationForm);
     //this.email = this.password = '';
   }
@@ -42,6 +85,11 @@ export class RegisterComponent implements OnInit {
     if (!control.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
       return {noEmail: true};
     }
+  }
+
+  onLogout() {
+    alert("LogOut Called");
+    this.authenticationService.logout();
   }
 
   isEqualPassword(control: FormControl): {[s: string]: boolean} {
