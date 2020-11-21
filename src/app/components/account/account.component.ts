2303c1acb60417936140f61eba204845e04226f2
ashaik16 <ashaik16@depaul.edu>
2020-11-21 11:59:42 -0600

    Changes for Rodolfo

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,23 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-
-export interface BankAccount {
-  accountnumber: number;
-  accounttype: string;
-  balance: number;
-}
-
-const ACCOUNT_DATA: BankAccount[] = [
-  { accountnumber: 1, accounttype: 'Checking', balance: 500 },
-  { accountnumber: 2, accounttype: 'Savings', balance: 500 },
-];
+import { MatTableDataSource } from '@angular/material/table';
+import { UserService } from '../../_services/user.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { DataSource } from '@angular/cdk/collections';
+import { User } from '../../models/user';
+import {TransferFundsService} from '../../_services/transferFunds.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent {
-  displayedColumns: string[] = ['accountnumber', 'accounttype', 'balance'];
-  dataSource = ACCOUNT_DATA;
 
+export class AccountComponent implements OnInit {
+ // dataSource = new UserDataSource(this.userService);
+  displayedColumns: string[] = ['accountnumber', 'accounttype', 'balance'];
+  accType: String;
+  //constructor(private userService: UserService) { }
+  constructor(private transferFundsService: TransferFundsService ) {
+    // ngOnInit() {
+    // }
+  
 }
+ngOnInit(): void {
+ 
+
+    //this.accType = accountType;
+  
+    this.transferFundsService.getAccountDetails().subscribe(
+      res => {
+        for (let i = 0; i < res.length; i++) {
+          if ((res[i].accountType) == this.accType) {
+  
+  
+          //	this.setValues(res, i);
+          }
+        }
+  
+      },
+      error => {
+        alert(error.error.message)
+  
+      }
+    );
+  }
+  
+}
+// export class UserDataSource extends DataSource<any> {
+//   constructor(private userService: UserService) {
+//     super();
+//   }
+  // connect(): Observable<User[]> {
+  //   return this.userService.getUser();
+  // }
+ //disconnect() { }
+//}
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: "https://group7-banking-system-backend.herokuapp.com",
-  firebaseConfig: {
-    apiKey: "AIzaSyAsYBbWCrbJTaO4ze0dt-RIY2EV7uQQNEs",
-    authDomain: "frontendbankingsystem.firebaseapp.com",
-    databaseURL: "https://frontendbankingsystem.firebaseio.com",
-    projectId: "frontendbankingsystem",
-    storageBucket: "frontendbankingsystem.appspot.com",
-    messagingSenderId: "696784028044",
-    appId: "1:696784028044:web:4711d6e06b9567563356a9",
-    measurementId: "G-TJSC3KW9M6"
-  }
+  baseUrl: "http://localhost:8080",
+
 
 };
 
