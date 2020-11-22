40b3ab383a4f9165e01fffcefff94dfcf73aa6df
ashaik16 <ashaik16@depaul.edu>
2020-11-21 18:15:19 -0600

    Account changes updated

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -8,17 +8,27 @@ import { DataSource } from '@angular/cdk/collections';
 import { User } from '../../models/user';
 import { TransferFundsService } from '../../_services/transferFunds.service';
 import { HttpClient } from '@angular/common/http';
+import { Accounts } from '../../models/Accounts';
+import {ViewChild, ElementRef} from '@angular/core';
+ import * as XLSX from 'xlsx';
+import { analytics } from 'firebase';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
 
-export class AccountComponent implements OnInit {
-  userService: UserService = new UserService();
-  dataSource = new UserDataSource(this.userService);
-  displayedColumns: string[] = ['accountnumber', 'accounttype', 'balance'];
+export class AccountComponent implements OnInit{
+  // userService: UserService = new UserService();
+  // dataSource = new UserDataSource(this.userService);
+  //@ViewChild('TABLE') table: ElementRef;
+  displayedColumns: string[] = ['id', 'accountType', 'balance'];
+  accountDetails=[];
+  //dataSource = accountDetails;
+
   accType: String;
+
+
   //constructor(private userService: UserService) { }
   constructor(private transferFundsService: TransferFundsService) {
     // ngOnInit() {
@@ -27,20 +37,23 @@ export class AccountComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+ 
 
     //this.accType = accountType;
 
     this.transferFundsService.getAccountDetails().subscribe(
       res => {
         for (let i = 0; i < res.length; i++) {
-          if ((res[i].accountType) == this.accType) {
-
-
-            //	this.setValues(res, i);
-          }
+          // this.accountDetails=res;
+          // this.dataSource = this.accountDetails;
+      ///    const result = res.map(item => Object.keys(item)[0]);
+        
+          //  this.accountDetails  = [
+          //   {id: res.map(item => Object.keys(item)[0]), accountType: res.map(item => Object.keys(item)[2]) , balance: res.map(item => Object.keys(item)[3])},
+          // ];
         }
-
+        this.accountDetails =res;
+      //  res[0].accountType
       },
       error => {
         alert(error.error.message)
@@ -50,13 +63,24 @@ export class AccountComponent implements OnInit {
   }
 
 }
-export class UserDataSource extends DataSource<any> {
-  constructor(private userService: UserService) {
-    super();
-  }
-  connect(): Observable<User[]> {
-    //return this.userService.getUser();
-  }
-  disconnect() { }
+export interface accountDetails {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
 }
+const accountDetails  = [
+  {id: res.map(item => Object.keys(item)[0]), accountType: res.map(item => Object.keys(item)[2]) , balance: res.map(item => Object.keys(item)[3])},
+  ];
+ 
+
+// export class UserDataSource extends DataSource<any> {
+//   constructor(private userService: UserService) {
+//     super();
+//   }
+//   connect(): Observable<User[]> {
+//     //return this.userService.getUser();
+//   }
+//   disconnect() { }
+// }
 
